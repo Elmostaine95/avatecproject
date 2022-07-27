f60f763698f453aaba426c53c134ddde1b915f36
Badr-Eddine El Mostaine <badr.elmostaine@gmail.com>
2022-07-27 09:23:01 +0100

    file manager
    v2.1

--- yt-auth-nodejs-main PostgreSQL/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-require("dotenv").config();
-
-
-const Permissions = require("./models/permissions");
-const Roles = require("./models/roles");
-const MenuPages = require("./models/menuPages");
-
-
